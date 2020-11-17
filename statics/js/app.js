d3d23c0d557782aa506b76d30425a21301020c64
Claus Juhl Knudsen <earkivar@gmail.com>
2020-11-17 07:49:22 +0100

    midstream

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,3 +1,21 @@
+function test_searchfilters() {
+    // Open filtersections root-"details"-tag in grid-view (min 46em)
+    // Hide filtersections "summary"-tag when in grid-view
+    let details = document.querySelector("#filterSection-details");
+    let summary = details.firstElementChild;
+
+    if (window.matchMedia(`(min-width: 46em)`).matches) {
+        details.setAttribute("open", "");
+        if (!summary.className.match(/(?:^|\s)sr-only(?!\S)/) ) {
+            summary.className += " sr-only";
+        }
+    } else {
+        details.removeAttribute("open");
+        summary.className = summary.className.replace( /(?:^|\s)sr-only(?!\S)/g , '' );
+    }
+}
+
+// Load
 window.addEventListener('load', function () {
     const skipLinks = document.querySelectorAll('.skipLink');
     for (let i = 0; i < skipLinks.length; i++) {
@@ -6,5 +24,10 @@ window.addEventListener('load', function () {
             document.getElementById(id_).focus();
         })
     }
-    document.querySelector("#searchfilters-details").setAttribute("open", "open");
+    test_searchfilters();
+});
+
+// Resize
+window.addEventListener('resize', function () {
+    test_searchfilters();
 });
--- statics/js/treeLinks.js
+++ statics/js/treeLinks.js
@@ -22,16 +22,24 @@ window.addEventListener('load', function () {
     t.init();
   }
   // Select all treeitem-span the functions as headers with subtrees
-  var subtreeHeaders = document.querySelectorAll('li[role="treeitem"] > span');
-  var stub = `<svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
-      <rect class="vert" height="7" width="1" y="1" x="4"/>
-      <rect height="1" width="7" y="4" x="1"/>
-      </svg>`
+  var subtreeHeaders = document.querySelectorAll('li[role="treeitem"] > span[class="facetItem"]');
+  // var stub = `<svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+  //     <rect class="vert" height="7" width="1" y="1" x="4"/>
+  //     <rect height="1" width="7" y="4" x="1"/>
+  //     </svg>`
+
+  // var stub = `<svg class="icon" aria-hidden="true" focusable="false"
+  //   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+  //   <path fill-rule="evenodd" d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"></path>
+  //   </svg>`
 
   for (var i = 0; i < subtreeHeaders.length; i++) {
     var h = subtreeHeaders[i];
-    var content = h.textContent;
-    h.innerHTML = content + stub;
+    // var content = h.textContent;
+    h.innerHTML += `<svg class="icon" aria-hidden="true" focusable="false"
+    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+    <path fill-rule="evenodd" d="M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z"></path>
+    </svg>`;
   }
 });
 
